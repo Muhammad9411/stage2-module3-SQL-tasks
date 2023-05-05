353c70e20181ec84363b8bdf04de3b7f396bf106
MuhammadJon <mtjohn9411@gmail.com>
2023-05-05 11:14:00 +0500

    task has been solution

--- lesson2/src/main/resources/CREATE.sql
+++ lesson2/src/main/resources/CREATE.sql
@@ -3,15 +3,15 @@ CREATE TABLE student
 (
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(45),
-    birthday DATE NOT NULL,
-    groupnumber INT NOT NULL
+    birthday DATE,
+    groupnumber INT
 );
 CREATE TABLE subject
 (
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(250),
     description VARCHAR(255),
-    grade int NOT NULL
+    grade int
 );
 
 CREATE TABLE mark
@@ -19,7 +19,7 @@ CREATE TABLE mark
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     student_id BIGINT,
     subject_id BIGINT,
-    mark INT NOT NULL,
+    mark INT,
     foreign key (student_id) references student(id),
     foreign key (subject_id) references subject(id)
 );
@@ -34,8 +34,8 @@ CREATE TABLE payment
 (
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     type_id BIGINT NOT NULL,
-    amount decimal NOT NULL,
-    payment_date TIMESTAMP  NOT NULL,
+    amount decimal,
+    payment_date TIMESTAMP,
     student_id BIGINT NOT NULL,
     foreign key (type_id) references paymenttype(id),
     foreign key (student_id) references student(id)
