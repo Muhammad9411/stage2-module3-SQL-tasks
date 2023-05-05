686bd93785d6aeb5d959f3c3d31773ffe2ee1947
MuhammadJon <mtjohn9411@gmail.com>
2023-05-05 10:23:18 +0500

    task has been solution

--- lesson1/src/main/resources/SOLUTION.sql
+++ lesson1/src/main/resources/SOLUTION.sql
@@ -2,6 +2,5 @@ CREATE TABLE student (id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHA
 CREATE TABLE subject (id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(250), description VARCHAR(255), grade int NOT NULL);
 CREATE TABLE paymenttype (id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(45));
 CREATE TABLE payment (id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, type_id BIGINT NOT NULL, amount decimal NOT NULL, payment_date TIMESTAMP  NOT NULL, student_id BIGINT NOT NULL, foreign key (type_id) references paymenttype(id), foreign key (student_id) references student(id));
-CREATE TABLE paymenttype (id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(45));
 CREATE TABLE mark (id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, student_id BIGINT, subject_id BIGINT, mark INT NOT NULL, foreign key (student_id) references student(id), foreign key (subject_id) references subject(id));
 
