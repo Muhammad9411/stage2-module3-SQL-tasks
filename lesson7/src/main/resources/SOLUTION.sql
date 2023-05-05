f15e30dfc72238a2308a30d1c8eb564e78dcda31
MuhammadJon <mtjohn9411@gmail.com>
2023-05-05 10:19:25 +0500

    task has been solution

--- lesson7/src/main/resources/SOLUTION.sql
+++ lesson7/src/main/resources/SOLUTION.sql
@@ -2,4 +2,4 @@ SELECT * FROM mark WHERE mark > 6 order by mark desc;
 SELECT * FROM payment WHERE amount < 300 order by amount asc;
 SELECT * FROM paymenttype order by name;
 SELECT * FROM student order by name desc;
-SELECT * FROM student WHERE id IN (select student_id from payment group by student_id having count (*) > 2) order by birthday asc;
+SELECT * FROM student WHERE id IN (select student_id from payment where amount > 1000) order by birthday asc;
--- lesson8/src/main/resources/SOLUTION.sql
+++ lesson8/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
-SELECT min (birthday) FROM student;
+SELECT max (birthday) FROM student;
 SELECT min (payment_date) FROM payment;
 SELECT avg (mark) FROM mark WHERE subject_id in (SELECT id FROM subject WHERE name = 'Math');
 SELECT min (amount) FROM payment WHERE type_id in (SELECT id FROM paymenttype WHERE name = 'WEEKLY');
