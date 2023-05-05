f68462ce2965f06759e61ab4e5dfb78b82e7f24b
MuhammadJon <mtjohn9411@gmail.com>
2023-05-05 13:29:18 +0500

    task has been solution

--- lesson12/src/main/resources/TABLES_CHANGES.sql
+++ lesson12/src/main/resources/TABLES_CHANGES.sql
@@ -1,6 +1,6 @@
 alter table mark drop constraint mark_student_id_fkey;
-ALTER TABLE mark ADD CONSTRAINT mark_student_id_fkey FOREIGN KEY(student_id) REFERENCES student(id) ON DELETE CASCADES;
+ALTER TABLE mark ADD CONSTRAINT mark_student_id_fkey FOREIGN KEY(student_id) REFERENCES student(id) ON DELETE CASCADE;
 alter table payment drop constraint payment_student_id_fkey;
-ALTER TABLE payment ADD CONSTRAINT payment_student_id_fkey FOREIGN KEY(student_id) REFERENCES student(id) ON DELETE CASCADES;
+ALTER TABLE payment ADD CONSTRAINT payment_student_id_fkey FOREIGN KEY(student_id) REFERENCES student(id) ON DELETE CASCADE;
 alter table payment drop constraint payment_type_id_fkey;
-ALTER TABLE payment ADD CONSTRAINT payment_type_id_fkey FOREIGN KEY(type_id) REFERENCES paymenttype(id) ON DELETE CASCADES;
+ALTER TABLE payment ADD CONSTRAINT payment_type_id_fkey FOREIGN KEY(type_id) REFERENCES paymenttype(id) ON DELETE CASCADE;
